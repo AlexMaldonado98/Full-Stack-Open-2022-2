fe1b0047e9a19877ddd8bcb52a54ff7c0ab53bdf
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-07-26 14:00:34 -0500

    exercise 3.3

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ let persons = [
 app.get('/api/persons',(request,response) => {
     response.json(persons);
 });
+
+app.get('/api/persons/:id',(request,response) => {
+    const id = Number(request.params.id)
+    const person = persons.find((person) => person.id === id )
+    if(person){
+        response.json(person);
+    }else{
+        response.status(404).end();
+    }
+});
+
 app.get('/info',(request,response) => {
     response.send(`
         <p>Phonebook has info for ${persons.length} people </p>
