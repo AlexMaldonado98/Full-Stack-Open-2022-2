ec2a13b1a0712a2181c122827f3d58dd23e3ec39
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-07-27 12:11:27 -0500

    exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
 
-app.use(express.json());
+app.use(express.json(),cors());
 
 const idGen = () => {
     const id = Math.round(Math.random() * 1000);
