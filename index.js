7ec39fa6c07e09f52192b931a526aafc82f3a9d5
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-07-27 13:26:44 -0500

    exercise 3.10

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.use((request, response) => {
     })
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 30001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
